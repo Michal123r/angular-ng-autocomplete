bfff7e9b0c4c255fd17e95ed188517e6a6bf3551
michal <michal12r@gmail.com>
2021-10-11 10:05:08 +0300

    clean code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -35,40 +35,5 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  onChangeSearch(term: string) {
-    console.log('term', term);
-    this.isLoading = true;
-    this._dataService.getRepos(term).subscribe(res => {
-      console.log('res', res);
-      this.items = this.items ? this.items.concat(res['items']) : res['items'];
-      this.items = res['items'];
-      this.isLoading = false;
-    }, (err) => {
-      console.log('err', err);
-      this.isLoading = false;
-    });
-  }
 
- 
- 
-  focusedEventApi(e) {
-    console.log('focused');
-    // Fetch API data on Load
-    this.onChangeSearch(null);
-  }
-
-
- 
-
-  scrollToEndApi() {
-    this.onChangeSearch('w');
-  }
-
-  customFilter(items: any, query: string) {
-    return items.filter((item: any) => {
-      return item.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
-    });
-  }
-
-  disableFilter = (items: any[]) => items;
 }
